527ae5fe680d0f784a1311d53cd2839d88a4b4c0
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-05-28 18:28:11 +0800

    init

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware('auth:api')->get('/user', function(Request $request) {
+    return $request->user();
+});
+
+// List Articles
+Route::get('articles', 'ArticleController@index');
+
+// List single article
+Route::get('article/{id}', 'ArticleController@show');
+
+// Create new article
+Route::post('article', 'ArticleController@store');
+
+// Update article
+Route::put('article', 'ArticleController@store');
+
+// Delete article
+Route::delete('article/{id}', 'ArticleController@destroy');
